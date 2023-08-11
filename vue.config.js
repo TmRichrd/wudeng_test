a7f51e47444b097ef6404f1e3bc615d39cb1cf1e
SaKItama <1084491297@qq.com>
2023-08-11 09:55:34 +0800

    fix🐛: api

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-
 const TerserPlugin = require('terser-webpack-plugin') // 用于在生成环境剔除debuger和console
 const CompressionPlugin = require('compression-webpack-plugin') // gzip压缩,优化http请求,提高加载速度
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin // 代码分析工具
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin // 代码分析工具
 const path = require('path')
-const resolve = dir => {
+const resolve = (dir) => {
   return path.join(__dirname, dir)
 }
 
@@ -23,14 +23,14 @@ module.exports = {
     host: '0.0.0.0',
     port: 3008,
     proxy: {
-      '/index.php': {
+      '/api': {
         target: target,
         changeOrigin: true,
         pathRewrite: {
-          '/api': ''
-        }
-      }
-    }
+          '/api': '',
+        },
+      },
+    },
   },
   // webpack相关配置
   chainWebpack: (config) => {
@@ -40,8 +40,7 @@ module.exports = {
       .set('@img', resolve('src/assets/images'))
       .set('cps', resolve('src/components'))
     // 关闭npm run build之后，This can impact web performance 警告
-    config.performance
-      .set('hints', false)
+    config.performance.set('hints', false)
     // 移除 prefetch 插件
     config.plugins.delete('prefetch')
     // 移除 preload 插件
@@ -50,7 +49,7 @@ module.exports = {
     config.optimization.minimize(true)
     // 分割代码
     config.optimization.splitChunks({
-      chunks: 'all'
+      chunks: 'all',
     })
     // 对图片进行压缩处理
     config.module
@@ -60,7 +59,7 @@ module.exports = {
       .options({
         disable: true, // webpack@2.x and newer
         quality: '65-80',
-        speed: 4
+        speed: 4,
       })
       .end()
     // 项目文件大小分析
@@ -70,10 +69,9 @@ module.exports = {
     //     analyzerPort: 3002
     //   }))
   },
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     // 为生产环境修改配置...
-    if (process.env.NODE_ENV === 'production')
-    {
+    if (process.env.NODE_ENV === 'production') {
       new TerserPlugin({
         cache: true,
         parallel: true,
@@ -83,34 +81,31 @@ module.exports = {
             warnings: false,
             drop_console: true,
             drop_debugger: true,
-            pure_funcs: ['console.log']
-          }
-        }
+            pure_funcs: ['console.log'],
+          },
+        },
       })
       // 开启gzip压缩
-      config.plugins.push(new CompressionPlugin({
-        algorithm: 'gzip',
-        test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'), // 匹配文件扩展名
-        // threshold: 10240, // 对超过10k的数据进行压缩
-        threshold: 5120, // 对超过5k的数据进行压缩
-        minRatio: 0.8,
-        cache: true, // 是否需要缓存
-        deleteOriginalAssets: false // true删除源文件(不建议);false不删除源文件
-      }))
-    } else
-    {
+      config.plugins.push(
+        new CompressionPlugin({
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'), // 匹配文件扩展名
+          // threshold: 10240, // 对超过10k的数据进行压缩
+          threshold: 5120, // 对超过5k的数据进行压缩
+          minRatio: 0.8,
+          cache: true, // 是否需要缓存
+          deleteOriginalAssets: false, // true删除源文件(不建议);false不删除源文件
+        })
+      )
+    } else {
       // 为开发环境修改配置...
-
     }
   },
   // 第三方插件配置
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
-      patterns: [
-        path.resolve(__dirname, './src/style/variables.less')
-      ]
-    }
-  }
+      patterns: [path.resolve(__dirname, './src/style/variables.less')],
+    },
+  },
 }
-
